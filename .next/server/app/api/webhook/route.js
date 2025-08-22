e85090a8eedf2143c3f1ea494358bae4d11bb011
SachinSharma-LordHeydes <thereisamailforme@gmail.com>
2025-08-23 00:00:24 +0545

    Remove .next and .env from repo

--- .next/server/app/api/webhook/route.js
+++
@@ -1,7 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/app/api/webhook/route.js";
-const runtime = require("../../../chunks/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/[root-of-the-server]__c317d2f9._.js");
-runtime.loadChunk("server/chunks/node_modules_c345caab._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/.next-internal/server/app/api/webhook/route/actions.js [app-rsc] (server actions loader, ecmascript)", CHUNK_PUBLIC_PATH);
-runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/webhook/route.tsx [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/webhook/route.tsx [app-route] (ecmascript)\" } [app-route] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/static/chunks/app_(main)_account_page_tsx_4581a78b._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_f0af0d05._.js",
-  "static/chunks/node_modules_f7a717ff._.js"
-],
-    source: "dynamic"
-});
